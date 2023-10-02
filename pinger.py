e5afb62ce5f9e38ecc89a661296b7be74f669db9
5ilvestris <117170752+5ilvestris@users.noreply.github.com>
2023-10-02 17:46:19 +0300

    Add files via upload

---
+++ pinger.py
@@ -0,0 +1,38 @@
+import argparse
+import socket
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+  "-p",
+  "--ping",
+  dest="ping",
+  type=str,
+  help="Pinglenecek site",
+)
+parser.add_argument(
+  "-o",
+  "--output",
+  dest="output",
+  type=str,
+  help="Pinglenecek site",
+)
+args = parser.parse_args()
+adress = args.ping
+
+
+
+
+socket.setdefaulttimeout(2)
+host = socket.gethostbyname(args.ping)
+print(host)
+
+cwd = os.getcwd()
+print(cwd)
+# nmap -sV --script=vulscan/vulscan.nse www.example.com
+
+os.system("nmap -sv --script="+cwd+"//vulscan//vulscan.nse "+host)
+
+
+
+
